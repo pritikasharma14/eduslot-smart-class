ceb13af7b5b2478b2f3b8189fe14361a9e75c4ff
pritika1397 <pritika1397.be23@chitkarauniversity.edu.in>
2026-09-25 11:09:17 +0530

    Configure MongoDB Atlas for deployment

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/seat", require("./routes/seat"));
 // MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/smartseat")
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
@@ -17,4 +17,8 @@ app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
